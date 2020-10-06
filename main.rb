9e13c3415767a1250b738a833358365d732c6d77
Ceejayski <61045640+Ceejayski@users.noreply.github.com>
2020-10-06 19:16:06 +0100

    Update README.md

--- main.rb
+++ main.rb
@@ -1,16 +1,18 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-    for index in (0..self.size-1) do
+
+    (0..size - 1).each do |index|
       yield(self[index])
     end
     self
   end
 
-  #--------my each by index ------- 
+  #--------my each by index -------
   def my_each_with_index
     return to_enum unless block_given?
-    for index in (0..self.size-1) do
+
+    (0..size - 1).each do |index|
       yield(self[index], index)
     end
     self
@@ -18,78 +20,80 @@ module Enumerable
 
   def my_select
     return to_enum unless block_given?
-    select = Array.new
-    self.my_each{|x| select.push(x) if yield(x)}
+
+    select = []
+    my_each { |x| select.push(x) if yield(x)}
     select
   end
-  
-  def my_all?(args=nil)
-    if !args.nil? # if arguments or method parameters were given 
+
+  def my_all?(args = nil)
+    if !args.nil? # if arguments or method parameters were given
       if args.is_a?(Class)
-        self.my_each{|i| return false unless i.is_a?(args)}
+        my_each { |i| return false unless i.is_a?(args)}
       elsif args.is_a?(Regexp)
-        self.my_each{|i| return false unless arg.match(i.to_s)}
+        my_each { |i| return false unless arg.match(i.to_s)}
       else
-        self.my_each{ |i| return false unless i == args}
+        my_each { |i| return false unless i == args}
       end
-    elsif block_given? #if block was given
-      self.my_each{|i| false unless yield(x)}
+    elsif block_given? # if block was given
+      my_each { |_i| false unless yield(x)}
     else
-      self.my_each{|i| false unless i}
+      my_each { |i| false unless i}
     end
     true
   end
 
-  def my_any?(args=nil)
-    if !args.nil? # if arguments or method parameters were given 
+  def my_any?(args = nil)
+    if !args.nil? # if arguments or method parameters were given
       if args.is_a?(Class)
-        self.my_each{|i| return true if i.is_a?(args)}
+        my_each { |i| return true if i.is_a?(args)}
       elsif args.is_a?(Regexp)
-        self.my_each{|i| return true if arg.match(i.to_s)}
+        my_each { |i| return true if arg.match(i.to_s)}
       else
-        self.my_each{ |i| return true if i == args}
+        my_each { |i| return true if i == args}
       end
-    elsif block_given? #if block was given
-      self.my_each{|i| true if yield(x)}
+    elsif block_given? # if block was given
+      my_each { |_i| true if yield(x)}
     else
-      self.my_each{|i| true if i}
+      my_each { |i| true if i}
     end
     false
   end
 
-  def my_none?(args=nil)
-    if !args.nil? # if arguments or method parameters were given 
+  def my_none?(args = nil)
+    if !args.nil? # if arguments or method parameters were given
       if args.is_a?(Class)
-        self.my_each{|i| return false if i.is_a?(args)}
+        my_each { |i| return false if i.is_a?(args)}
       elsif args.is_a?(Regexp)
-        self.my_each{|i| return false if arg.match(i.to_s)}
+        my_each { |i| return false if arg.match(i.to_s)}
       else
-        self.my_each{ |i| return false if i == args}
+        my_each { |i| return false if i == args}
       end
-    elsif block_given? #if block was given
-      self.my_each{|i| false if yield(x)}
+    elsif block_given? # if block was given
+      my_each { |_i| false if yield(x)}
     else
-      self.my_each{|i| false if i}
+      my_each { |i| false if i}
     end
     true
   end
 
-  def my_count(args=nil)
+  def my_count(args = nil)
     total = 0
-    if block_given? #block was stated and args is nil
-      self.my_each{|i| total += 1 if yield(i)}
-    elsif !args.nil?#block wasn't stated and args isnot nil
-      self.my_each{|i| total += 1 if i == args}
+    if block_given? # block was stated and args is nil
+      my_each { |i| total += 1 if yield(i)}
+    elsif !args.nil? # block wasn't stated and args isnot nil
+      my_each { |i| total += 1 if i == args}
     else
-      return self.size
+      return size
     end
     total
   end
 
   def my_map(&block)
-    return to_enum(:my_map) if !block_given?
+    return to_enum(:my_map) unless block_given?
+
     mapped = []
-    self.my_each { |i| mapped << block.call(i)}
+    my_each { |i| mapped << block.call(i)}
     mapped
   end
 
@@ -113,29 +117,28 @@ module Enumerable
 end
 
 def multiply_els(array)
-  array.my_inject(1) {|multiple, i| multiple*i}
+  array.my_inject(1) { |multiple, i| multiple * i}
 end
 
+[1, 2, 3, 4, 5].my_each { |x| puts x}
 
-[1,2,3,4,5].my_each {|x| puts x}
+[1, 2, 3, 4, 5].my_each_with_index { |x, y| puts "#{x} and #{y}"}
 
-[1,2,3,4,5].my_each_with_index {|x,y| puts "#{x} and #{y}"}
+[1, 2, 3, 4, 5].my_select { |x| puts x if x.even? }
 
-[1,2,3,4,5].my_select {|x| puts x if x%2 ==0 }
+puts [1, 2, 3, 4, 5].my_all?(Integer)
 
-puts [1,2,3,4,5].my_all?(Integer)
+puts [1, 2, 3, 4, 5].my_any?(Integer)
 
-puts [1,2,3,4,5].my_any?(Integer)
+puts [1, 2, 3, 4, 5].my_none?(Integer)
 
-puts [1,2,3,4,5].my_none?(Integer)
+puts [1, 2, 3, 4, 5, 3, 3, 3, 3, 3, 3].my_count
 
-puts [1,2,3,4,5,3,3,3,3,3,3].my_count
-
-print [1,2,3,4,5,3,3,3,3,3,3].my_map {|x| x+3}
-
-puts
-
-print [1,2,3,4,5,3,3,3,3,3,3].my_inject(:+)
+print [1, 2, 3, 4, 5, 3, 3, 3, 3, 3, 3].my_map { |x| x + 3}
 
 puts
-puts multiply_els([1,2,3,4,5,3,3,3,3,3,3])
+
+print [1, 2, 3, 4, 5, 3, 3, 3, 3, 3, 3].my_inject(:+)
+
+puts
+puts multiply_els([1, 2, 3, 4, 5, 3, 3, 3, 3, 3, 3])
